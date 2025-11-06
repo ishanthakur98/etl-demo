1ad524cd2711660872c59cd777e6dc776a0f8645
ishan thakur <ishan1998thakur@gmail.com>
2025-11-06 20:24:57 +0530

    removed cron jobs

--- batch-launcher/src/main/java/com/example/LauncherApplication.java
+++ batch-launcher/src/main/java/com/example/LauncherApplication.java
@@ -8,8 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @SpringBootApplication(scanBasePackages = "com.example") // Scan all sub-modules
-@EnableScheduling
-@EnableAsync
 public class LauncherApplication {
     public static void main(String[] args) {
         SpringApplication.run(LauncherApplication.class, args);
